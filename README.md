102bbc36e95c9e46c7def94434fe486fdb16f7c7
Elie Hanna <alps.elie@gmail.com>
2023-03-15 09:52:23 +0200

    added regex to scrape html

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 <h2>Import</h2>
 First, I imported requests to be able to get the html of the url, and sys to get the argument (url).
+I also imported re to use regex to scrape the html code.
--- test_script.py
+++ test_script.py
@@ -1,8 +1,12 @@
 #Elie Hanna
 import requests
 import sys
+import re
 
 arg_list = sys.argv #list that holds arguments
 url = arg_list[1] #string that holds 1st argumeny, the url
+
 r = requests.get(url) 
 html = r.text #holds the html of the url
+
+urls = re.findall(r'href=[\'"]?([^\'" >]+)', html) #matches strings that start with 'href=' and puts them all in a list, these strings are the urls
