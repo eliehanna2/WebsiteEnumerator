891498d7d11252e6e389701af83458f4a762a990
Elie Hanna <alps.elie@gmail.com>
2023-03-15 10:18:34 +0200

    directories done

--- test_script.py
+++ test_script.py
@@ -10,3 +10,16 @@ r = requests.get(url)
 html = r.text #holds the html of the url
 
 urls = re.findall(r'href=[\'"]?([^\'" >]+)', html) #matches strings that start with 'href=' and puts them all in a list, these strings are the urls
+
+subdomains_output  = open("./output_files/subdomains_output.bat", "a") #file that holds the correct subdomains
+directories_output  = open("./output_files/directories_output.bat", "a") #file that holds the correct directories
+
+with open('./input_files/dirs_dictionary.bat', 'r') as file: #read dirs_dictionary input file
+    for line in file: #iterate over given directories
+        full_url = url+line
+        get = requests.get(full_url)
+        #if the request succeeds, it means the url is valid
+        if get.status_code == 200:
+            directories_output.write(full_url) #append the url to directories_output file 
+
+
